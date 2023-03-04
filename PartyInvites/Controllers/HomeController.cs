8fff8bf150e08c37fc1b30c1159dddc2901b0dfa
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-04 10:58:37 +0100

    page 44

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace PartyInvites.Controllers
 
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
             return View("MyView");
         }
 
